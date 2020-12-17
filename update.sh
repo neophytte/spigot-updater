661899b53541bf5a90639499842b9939cd16f264
neophytte <neophytte@westnet.com.au>
2020-12-17 22:50:10 +0800

    Added comments

--- update.sh
+++ update.sh
@@ -1,27 +1,48 @@
 #!/bin/bash
 
-start=start.sh
-bak=start-pre-$1
-prev=previous_versions
-url=https://cdn.getbukkit.org/spigot/spigot-"$1".jar
-wget --spider $url
-echo "removing last backup ..."
-sudo rm -rf /home/minecraft-pre-update
-echo "backing up current version"
-sudo cp -R /home/minecraft /home/minecraft-pre-update
+# This script will update a spigot server 
+
+# Define the startup script
+START=start.sh
+# Define the backup for the startup script (in case of rollback) 
+BAK=startup-script-pre-$1
+# Define where to store the previous version (in case of rollback)
+PREV=previous_versions
+# Define the URL to grab
+URL=https://cdn.getbukkit.org/spigot/spigot-"$1".jar
+HOME=/home/minecraft
+HOME_BACKUP=/home/minecraft-pre-update
+
+# Script starts here
+
+# Check URL
+wget --spider $URL
+# Continue if the URL exists and the input is a number
 if [ $? -gt -1 ] && [ -n "$1" ] ; then
-	echo "moving `ls s*.jar` to $prev"
-	sudo mv s*.jar $prev/.
-	echo "retrieving $url"
-	sudo wget -nc $url
-	sudo cp $start $prev/$bak 
-	sudo chmod 400 $prev/$bak
-	sudo chmod 777 $start 
-	sudo echo "#!/bin/bash" > $start
-	sudo echo "sudo java -Xmx6G -Xms4G -jar /home/minecraft/spigot-$1.jar nogui > /dev/console" >> $start
-	sudo chmod 755 $start
+  echo "Removing last backup ... ($HOME_BACKUP)"
+  sudo rm -rf $HOME_BACKUP
+  echo "Backing up current version ($HOME)"
+  sudo cp -R $HOME $HOME_BACKUP
+	echo "moving `ls s*.jar` to $PREV"
+	sudo mv s*.jar $PREV/.
+	echo "retrieving $URL"
+  # Get the new version of Spigot
+	sudo wget -nc $URL
+  # Copy and update file properties
+	sudo cp $START $PREV/$BAK 
+	sudo chmod 400 $PREV/$BAK
+  # Modify the start script
+	sudo chmod 777 $START 
+	sudo echo "#!/bin/bash" > $START
+	sudo echo "sudo java -Xmx6G -Xms4G -jar /home/minecraft/spigot-$1.jar nogui > /dev/console" >> $START
+	sudo chmod 755 $START
 	echo "'sudo reboot' to take effect with version $1"
+  # Done!
 else
-	echo "needs version number on invoke"
-	echo "example $0 1.10"
+	if [ -n "$1" ]; then
+    echo "Error: Version number $1 doesn't exist at $URL"
+  else
+    echo "Error: Needs version number on invoke"
+  	echo "example $0 1.10"
+  fi
 fi
